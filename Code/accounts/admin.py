e6cebcaf8aac1b3c30d4f5220b950529620f8c76
niranjana-3 <nsax2990@uni.sydney.edu.au>
2024-08-29 21:22:29 +1000

    set up ability to populate database from admin site

--- Code/accounts/admin.py
+++ Code/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SalonAddress, UserType, Users, SalonInfo, CustomerInfo
 
-# Register your models here.
+admin.site.register(SalonAddress)
+admin.site.register(UserType)
+admin.site.register(Users)
+admin.site.register(SalonInfo)
+admin.site.register(CustomerInfo)
