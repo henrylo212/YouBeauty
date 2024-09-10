e7e339094edacc72b6d3ecc0db3aa4835ee30daf
henry <chlo8536@uni.sydney.edu.au>
2024-09-10 17:28:41 +1000

    removed incorrect settings.py

--- Code/youbeauty/settings.py
+++ Code/youbeauty/settings.py
@@ -118,3 +118,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # redirects user to home page when they login in successfully
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'capstone3888@gmail.com'
+# EMAIL_HOST_PASSWORD = 'Youbeauty'
+
+# DEFAULT_FROM_EMAIL = 'capstone3888@gmail.com'
