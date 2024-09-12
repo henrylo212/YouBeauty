e78f66e0d8990d690e80b8048b88ec7d7c5d00a0
Oydan <oydan@Oydans>
2024-09-12 22:08:44 +1000

    made individual pages for each salon that can be accessed by pressing the salon cards in the homepage

--- Code/salons/views.py
+++ Code/salons/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from .models import SalonInfo
+from django.shortcuts import render, get_object_or_404
+from accounts.models import SalonInfo
 
 def salon_list(request):
     salons = SalonInfo.objects.all()  
     return
+
+def salon_details(request, salon_id):
+    salon = get_object_or_404(SalonInfo, pk=salon_id)
+    return render(request, 'salons/salon_page.html', {'salon': salon})
--- Code/youbeauty/urls.py
+++ Code/youbeauty/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path('', include("accounts.urls")),
+    path('salon/', include('salons.urls')),
 ]
