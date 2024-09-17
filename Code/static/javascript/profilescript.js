c1a8d0ed3267dbbe75f27762b0c2b3e06715aee3
Katholiki Kritharides <kkri6877@uni.sydney.edu.au>
2024-09-18 00:58:52 +1000

    more form formatting + trying to get buttons showing properly

--- Code/static/javascript/profilescript.js
+++ Code/static/javascript/profilescript.js
@@ -1,15 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     const editButton = document.getElementById('edit-button');
+    const confirmButton = document.getElementById('confirm');
     const inputs = document.querySelectorAll('#profile-form input');
 
     console.log(editButton);
+    console.log(confirmButton);
     console.log(inputs);
+
+    // confirmButton.style.display = 'none';
 
     editButton.addEventListener('click', function() {
         inputs.forEach(input => {
             input.disabled = false;
         });
         editButton.style.display = 'none';
+    })
+
+    confirmButton.addEventListener('click', function() {
+        inputs.forEach(input => {
+            input.disabled = false;
+        });
+        // confirmButton.style.display = 'inline';
+        // confirmButton.style.display = 'inline-block';
+        // confirmButton.style.display = 'block';
+
+        // confirmButton.style.display = 'unset';
+        // confirmButton.style.display=
+        // confirmButton.style.display = "";
+        // confirmButton.style.contentVisibility = "visible";
+        // confirmButton.style.display = initial;
     });
 });
 
