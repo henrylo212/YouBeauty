298481cbae7d34e1699b0152dedc8ea8a98aee89
Katholiki Kritharides <kkri6877@uni.sydney.edu.au>
2024-09-18 20:11:13 +1000

    resizing of screen compatibility

--- Code/static/javascript/profilescript.js
+++ Code/static/javascript/profilescript.js
@@ -35,25 +35,4 @@ document.addEventListener('DOMContentLoaded', function() {
     confirmButton.addEventListener('click', function() {
         inputs.forEach(input => {
             input.disabled = false;
-        });
-        // confirmButton.style.display = 'inline';
-        // confirmButton.style.display = 'inline-block';
-        // confirmButton.style.display = 'block';
-
-        // confirmButton.style.display = 'unset';
-        // confirmButton.style.display=
-        // confirmButton.style.display = "";
-        // confirmButton.style.color = 'red';
-        // confirmButton.style.contentVisibility = 'visible';
-        confirmButton.style.visibility = 'visible';
-        confirmButton.style.width = '100%';
-        confirmButton.style.overflow = 'auto';
-
-        // confirmButton.style.display = initial;
-    });
-});
-
-
-// function editProfile(element, color) {
-//     element.style.backgroundcolor = color;
-// }
+   
