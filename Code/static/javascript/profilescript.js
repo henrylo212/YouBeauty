908470ef5d9f16295892aa9e0cef447ee43cc839
Katholiki Kritharides <kkri6877@uni.sydney.edu.au>
2024-09-18 18:18:22 +1000

    improving readability, adjusting margins

--- Code/static/javascript/profilescript.js
+++ Code/static/javascript/profilescript.js
@@ -2,18 +2,29 @@ document.addEventListener('DOMContentLoaded', function() {
     const editButton = document.getElementById('edit-button');
     const confirmButton = document.getElementById('confirm');
     const inputs = document.querySelectorAll('#profile-form input');
+    const nameInput = document.getElementById('profile-name');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone-number');
 
     console.log(editButton);
     console.log(confirmButton);
     console.log(inputs);
+    console.log(nameInput);
+    console.log(emailInput);
+    console.log(phoneInput);
 
     // confirmButton.style.display = 'none';
 
     editButton.addEventListener('click', function() {
         inputs.forEach(input => {
             input.disabled = false;
-            // input.style.
         });
+
+        nameInput.style.minWidth = "9em";
+        emailInput.style.minWidth = "9em";
+        phoneInput.style.minWidth = "9em";
+
+
         editButton.style.display = 'none';
         confirmButton.style.visibility = 'visible';
         confirmButton.style.width = '100%';
