54260b9c6b38bb829803bad801c54a5442b44ab2
Katholiki Kritharides <kkri6877@uni.sydney.edu.au>
2024-09-23 00:12:13 +1000

    test restructuring

--- Code/accounts/tests/test_models.py
+++ Code/accounts/tests/test_models.py
@@ -1,20 +1,21 @@
-from django.test import TestCase
-from ..models import UserType
-from ..models import SalonAddress
+# from django.test import TestCase
+# # from ..models import UserType
+# from django.contrib.auth.models import User
+# from accounts.models import SalonAddress
 
-class UserTypeTest(TestCase):
-    def test_model_creation(self):
-        """Test that a UserType object can be created successfully"""
-        user_type = UserType.objects.create(type='admin')
-        self.assertEqual(user_type.type, 'admin')
-        self.assertIsNotNone(user_type.type_id)  # Check that type_id is auto-assigned
+# # class UserTypeTest(TestCase):
+# #     def test_model_creation(self):
+# #         """Test that a UserType object can be created successfully"""
+# #         user_type = UserType.objects.create(type='admin')
+# #         self.assertEqual(user_type.type, 'admin')
+# #         self.assertIsNotNone(user_type.type_id)  # Check that type_id is auto-assigned
 
-class SalonAddressTest(TestCase):
-    def test_model_creation(self):
-        """Test that a SalonAddress object can be created successfully"""
-        salon_address = SalonAddress.objects.create(street='Cleveland St', suburb='Chippendale', state='NSW', postcode="2008")
-        self.assertEqual(salon_address.street, 'Cleveland St')
-        self.assertEqual(salon_address.suburb, 'Chippendale')
-        self.assertEqual(salon_address.state, 'NSW')
-        self.assertEqual(salon_address.postcode, '2008')
-        self.assertIsNotNone(salon_address.address_id)  # Check that address_id is auto-assigned
+# class SalonAddressTest(TestCase):
+#     def test_model_creation(self):
+#         """Test that a SalonAddress object can be created successfully"""
+#         salon_address = SalonAddress.objects.create(street='Cleveland St', suburb='Chippendale', state='NSW', postcode="2008")
+#         self.assertEqual(salon_address.street, 'Cleveland St')
+#         self.assertEqual(salon_address.suburb, 'Chippendale')
+#         self.assertEqual(salon_address.state, 'NSW')
+#         self.assertEqual(salon_address.postcode, '2008')
+#         self.assertIsNotNone(salon_address.address_id)  # Check that address_id is auto-assigned
---
+++ Code/accounts/tests/test_views.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import reverse
+# from ..views import UserType
+from django.contrib.auth.models import User
+# from salons.views import SalonAddress
+
+class RegistrationViewTest(TestCase):
+    def test_RegistrationView(self):
+        client_response = self.client.get(reverse("RegistrationView"))
+        self.assertEqual(client_response.status_code, 200)
