d5f60325308653e89109213298fe4e971782ca22
Katholiki Kritharides <kkri6877@uni.sydney.edu.au>
2024-10-09 21:29:46 +1100

    access to profile sorted

--- Code/accounts/context_processors.py
+++ Code/accounts/context_processors.py
@@ -6,9 +6,7 @@ def global_variable(request):
     current_user = request.user
     if current_user.is_authenticated:
         customer_exists = Customer.objects.filter(user=current_user).exists()
-        is_authenticated_customer = True
 
-
-    return {
-        "is_authenticated_customer": is_authenticated_customer
-    }
+        matching_customer_set = Customer.objects.filter(user=current_user)
+        num_matches = 0
+        last_customer_ma
