a92191dba3ff6fbe09f458ffc5b5a66ebdb2c543
Claudia Choi <ccho2041@uni.sydney.edu.au>
2024-10-10 21:47:04 +1100

    just a small change before heading off

--- Code/static/javascript/script.js
+++ Code/static/javascript/script.js
@@ -1,129 +1,54 @@
-const wrapperLocation = document.querySelector(".wrapper-location"),
-selectBtn = wrapperLocation.querySelector(".select-btn"),
-searchInp = wrapperLocation.querySelector("input"),
-options = wrapperLocation.querySelector(".options");
+const serviceWrapper = document.querySelector(".wrapper-service"),
+serviceSearch = serviceWrapper.querySelector(".service-form"),
+optionsSerivce = serviceWrapper.querySelector(".options");
+const hideService = serviceWrapper.querySelector(".hide"),
+services_list = serviceWrapper.querySelectorAll("li");
+let temp_services = Array.from(services_list);
 
-let countries = ["Camperdown", "Darlington", "Chippendale", "Hornsby", "Epping", "Strathfield", "Burwood", "Chatswood"]
-
-function addCountry() {
-    options.innerHTML = "";
-    countries.forEach(country => {
-        let li = `<li onclick="updateName(this)">${country}</li>`;
-        options.insertAdjacentHTML("beforeend", li);
-    });
-}
-addCountry();
-
-function updateName(selectedLi) {
-    searchInp.value = "";
-    addCountry();
-    wrapperLocation.classList.remove("active");
-    selectBtn.firstElementChild.innerText = selectedLi.innerText;
-}
-
-searchInp.addEventListener("keyup", () => {
-    let arr= [];
-    let searchedVal = searchInp.value.toLowerCase();
-    arr = countries.filter(data => {
-        return data.toLowerCase().startsWith(searchedVal);
-    }).map(data => `<li onclick="updateName(this)">${data}</li>`).join("");
-    options.innerHTML = arr;
+let services = [];
+temp_services.forEach(element => {
+    services.push(element.innerHTML.trim())
 });
-
-selectBtn.addEventListener("click", () => {
-    wrapperLocation.classList.toggle("active");
-    wrapperService.classList.remove("active");
-    salonNames.classList.remove("active");
-    // wrapperTime.classList.remove("active");
-});
-
-// Service Search
-const wrapperService = document.querySelector(".wrapper-service"),
-serviceBtn = wrapperService.querySelector(".service-btn"),
-searchInpService = wrapperService.querySelector("input"),
-optionsService = wrapperService.querySelector(".options");
-
-let services = ["Basic Cut", "Blowdry", "Wash and Cut", "Perm", "Bleach", "Dye", "Straighten", "Curl"]
 
 function addService() {
-    optionsService.innerHTML = "";
+    optionsSerivce.innerHTML = "";
     services.forEach(service => {
-        let li = `<li onclick="updateServiceName(this)">${service}</li>`;
-        optionsService.insertAdjacentHTML("beforeend", li);
+        let li = `<li onclick="updateService(this)">${service}</li>`;
+        optionsSerivce.insertAdjacentHTML("beforeend", li);
     });
 }
 addService();
 
-function updateServiceName(selectedLi) {
-    searchInpService.value = "";
+function updateService(selectedLi) {
+    serviceSearch.value = "";
     addService();
-    wrapperService.classList.remove("active");
-    serviceBtn.firstElementChild.innerText = selectedLi.innerText;
+    serviceWrapper.classList.remove("active");
+    serviceSearch.firstElementChild.innerText = selectedLi.innerText;
 }
 
-searchInpService.addEventListener("keyup", () => {
+serviceSearch.addEventListener("keyup", () => {
     let arr= [];
-    let searchedVal = searchInpService.value.toLowerCase();
+    let searchedVal = serviceSearch.value.toLowerCase();
     arr = services.filter(data => {
         return data.toLowerCase().startsWith(searchedVal);
-    }).map(data => `<li onclick="updateServiceName(this)">${data}</li>`).join("");
-    optionsService.innerHTML = arr;
+    }).map(data => `<li onclick="updateService(this)">${data}</li>`).join("");
+    optionsSerivce.innerHTML = arr;
 });
 
-serviceBtn.addEventListener("click", () => {
-    wrapperService.classList.toggle("active");
-    wrapperLocation.classList.remove("active");
+serviceSearch.addEventListener("click", () => {
+    serviceWrapper.classList.toggle("active");
+    // wrapperLocation.classList.remove("active");
     salonNames.classList.remove("active");
-    // wrapperTime.classList.remove("active");
 });
 
-
-// Time Search
-// const wrapperTime = document.querySelector(".wrapper-time"),
-// timeBtn = wrapperTime.querySelector(".time-btn"),
-// searchInpTime = wrapperTime.querySelector("input"),
-// optionsTime = wrapperTime.querySelector(".options");
-
-// let times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
-
-// function addTime() {
-//     optionsTime.innerHTML = "";
-//     times.forEach(time => {
-//         let li = `<li onclick="updateTimeName(this)">${time}</li>`;
-//         optionsTime.insertAdjacentHTML("beforeend", li);
-//     });
-// }
-// addTime();
-
-// function updateTimeName(selectedLi) {
-//     searchInpTime.value = "";
-//     addTime();
-//     wrapperTime.classList.remove("active");
-//     timeBtn.firstElementChild.innerText = selectedLi.innerText;
-// }
-
-// searchInpTime.addEventListener("keyup", () => {
-//     let arr= [];
-//     let searchedVal = searchInpTime.value.toLowerCase();
-//     arr = times.filter(data => {
-//         return data.toLowerCase().startsWith(searchedVal);
-//     }).map(data => `<li onclick="updateTimeName(this)">${data}</li>`).join("");
-//     optionsTime.innerHTML = arr;
-// });
-
-// timeBtn.addEventListener("click", () => {
-//     wrapperTime.classList.toggle("active");
-//     wrapperLocation.classList.remove("active");
-//     wrapperService.classList.remove("active");
-// });
 
 
 const salonNames = document.querySelector(".search-keywords"),
 salonSearch = salonNames.querySelector(".search-form"),
 optionsSalon = salonNames.querySelector(".options");
 const hide = salonNames.querySelector(".hide"),
-services_list = salonNames.querySelectorAll("li");
-let temp_salonNames = Array.from(services_list);
+salon_list = salonNames.querySelectorAll("li");
+let temp_salonNames = Array.from(salon_list);
 
 let salon_names = [];
 temp_salonNames.forEach(element => {
@@ -157,6 +82,6 @@ salonSearch.addEventListener("keyup", () => {
 
 salonSearch.addEventListener("click", () => {
     salonNames.classList.toggle("active");
-    wrapperLocation.classList.remove("active");
-    wrapperService.classList.remove("active");
+    // wrapperLocation.classList.remove("active");
+    serviceWrapper.classList.remove("active");
 });
