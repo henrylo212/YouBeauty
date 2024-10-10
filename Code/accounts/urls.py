58619eecf35afad8bd979c5b89c60ca9656495a1
Mohitha Mohan <mmoh4694@uni.sydney.edu.au>
2024-10-10 19:22:36 +1100

    Fixed business login issues and logout stuff.

--- Code/accounts/urls.py
+++ Code/accounts/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+# from .views import CustomerLoginView, BusinessLoginView
 
 # Can raise and use views to give error messages too!!
 
@@ -16,14 +17,23 @@ urlpatterns = [
     path('bookings/', views.bookingsView, name = 'bookings'),
 
     path("forBusiness/", views.forBusinessView, name="forBusiness"),
-    path('salon-login/', views.salon_login, name='salon_login'),
+    # path('salon-login/', views.salon_login, name='salon_login'),
     path('business-register/', views.RegistrationView, name="business_register"),
     
+    path("login/", views.CustomerLoginView.as_view(), name='login'),
+    path("business_login/", views.BusinessLoginView.as_view(), name='business_login'),
+    
+    # path("logout/", views.CustomerLoginView.as_view(), name='login'),
+    # path("business_logout/", views.BusinessLoginView.as_view(), name='business_login'),
+
+    # path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
+    # path("business_login/", auth_views.LoginView.as_view(template_name="registration/business_login.html"), name='business_login'),
+
+    # These are all built in views!
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     path('search_results', views.search_results, name='search_results'),
-
 ]
