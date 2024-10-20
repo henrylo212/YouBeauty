7618f09196268f42c8d4d936ebf8e74e34d64e6b
Oydan <oydan@Oydans>
2024-10-20 22:16:38 +1100

    fixed bug in making bookings

--- Code/customers/views.py
+++ Code/customers/views.py
@@ -97,74 +97,6 @@ def TopSalonsView(request):
     return render(request, 'salons/top_salons.html', {'salons': salon_page})
 
 
-# @login_required
-# def make_bookings(request, salon_service_id):
-#     '''
-#     Handles information about the make bookings page
-
-#     This view handlers information about the make bookings page
-
-#     Args:
-#         request (HttpRequest): The HTTP request object, which can be GET or POST
-
-#     Template:
-#         'bookings/make_bookings.html': The template used to display the make bookings page
-    
-#     '''
-#     salon_service = SalonService.objects.get(id=salon_service_id)
-#     print('Helooo?')
-#     if request.method == 'POST':
-#         print('Confirm booking button clicked!')
-#         try:
-#             # Check if a Customer object exists for the current user
-#             customer = Customer.objects.get(user=request.user)
-#         except Customer.DoesNotExist:
-#             # Handle the case where the customer doesn't exist
-#             print("Customer does not exist for this user. Redirecting to the registration page.")
-#             # You could redirect the user to a page where they can create a customer profile
-#             # return redirect('create_customer_profile')
-
-#         # Retrieve form data
-#         date = request.POST.get('date')
-#         start_time_str = request.POST.get('start_time')
-
-#         # Parse the start time and service duration
-#         start_time = datetime.strptime(start_time_str, '%H:%M').time()
-#         service_duration = salon_service.duration  # Assuming duration is a timedelta
-
-#         # Calculate the end time
-#         start_datetime = datetime.combine(datetime.now().date(), start_time)
-#         end_datetime = start_datetime + service_duration
-#         end_time = end_datetime.time()
-
-
-#         # Create a new booking
-#         customer = Customer.objects.get(user=request.user)
-#         booking = Booking.objects.create(
-#             customer=customer,
-#             salon_service=salon_service,
-#             date=date,
-#             start_time=start_time,
-#             end_time=end_time,
-#             is_cancelled=False
-#         )
-#         print(f"Booking created for {Customer.user} on {date} at {start_time}")
-
-#         subject = f"Booking confirmed for {booking.customer.user.username} at {salon_service.salon.salon_name}"
-#         message = (
-#             f"Dear {booking.customer.user.username},\n\n"
-#             f"Thank you for booking with Youbeauty. Your {booking.salon_service} booking "
-#             f"on {date}, {start_time_str} at {salon_service.salon.salon_name} is confirmed.\n\n"
-#             f"Sincerely,\nYoubeauty"
-#         )
-#         recipient_list = [customer.user.email]
-#         send_mail(subject, message, 'your_email@example.com', recipient_list)
-
-
-#         return redirect('booking_confirmation', booking_id=booking.id)
-    
-#     return render(request, 'bookings/make_bookings.html', {'salon_service': salon_service})
-
 
 @login_required
 def make_bookings(request, salon_service_id):
@@ -180,8 +112,6 @@ def make_bookings(request, salon_service_id):
         'bookings/make_bookings.html': The template used to display the make bookings page
     '''
     salon_service = SalonService.objects.get(id=salon_service_id)
-    print('Helooo?')
-    # messages.error(request, 'This is a test error message.')  # This should always show
 
     if request.method == 'POST':
         print('Confirm booking button clicked!')
